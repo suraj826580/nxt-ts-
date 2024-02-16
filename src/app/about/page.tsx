ed9697f844264d683ee34f504b635c7637a7cc3d
Suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2024-02-17 00:53:37 +0530

    nested layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+export const metadata = {
+  title: "About Page",
+};
 
 function About() {
   return <h1>About</h1>;
--- src/app/products/[productID]/page.tsx
+++ src/app/products/[productID]/page.tsx
@@ -1,8 +1,22 @@
 import ReviewButton from "@/components/ReviewButton";
+import { Metadata } from "next";
 import React from "react";
 interface params {
   productID: String;
 }
+
+type props = {
+  params: {
+    productID: String;
+  };
+};
+export const generateMetaData = ({ params }: props): Metadata => {
+  return {
+    title: `productID ${params.productID}`,
+  };
+};
+// above code is mpt  working
+
 function Product({ params }: { params: params }) {
   const { productID } = params;
   return (
