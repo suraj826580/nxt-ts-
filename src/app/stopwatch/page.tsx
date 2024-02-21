cd07b27ac4e06bf0c5ffe014ffd9d01b7f38febd
Suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2024-02-22 00:37:04 +0530

    Stopwatch

--- src/app/stopwatch/page.tsx
+++ src/app/stopwatch/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 
-function Stopwatch() {
-  return <div>Dynamic Stopwatch</div>;
+function StopWatch(): JSX.Element {
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const intervalRef = useRef<number | NodeJS.Timeout | null>(null);
+
+  const handleStart = () => {
+    const currentTime = Date.now();
+    setNow(currentTime);
+    setStartTime(currentTime);
+    if (intervalRef.current !== null)
+      clearInterval(intervalRef.current as NodeJS.Timeout);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  };
+
+  let a = 0;
+  if (startTime !== null && now !== null) {
+    a = (now - startTime) / 1000;
+  }
+
+  return (
+    <>
+      <div>StopWatch : {a.toFixed(3)}</div>
+      <button onClick={handleStart}>Start</button>
+    </>
+  );
 }
 
-export default Stopwatch;
+export default StopWatch;
